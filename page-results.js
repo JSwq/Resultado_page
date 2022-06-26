5ca828326a3a244172473119f85e91f38dcc4cce
JSwq <90485843+JSwq@users.noreply.github.com>
2022-06-26 13:11:04 -0500

    Update page-results.js

--- page-results.js
+++ page-results.js
@@ -1,3 +1,6 @@
 function Reddirecct_page(){
     window.location.href = "https://jswq.github.io/Foro_page_co/foro_page.html";
 }
+function Reddirecct_page_foro(){
+    window.location.href = "https://jswq.github.io/user_perfil/usuario_page.html";
+}
